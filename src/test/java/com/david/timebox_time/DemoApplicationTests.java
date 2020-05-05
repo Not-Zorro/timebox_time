c2afa8a94445dd069ac56a9a1095da59d9a77146
David Lopez <ldavid.x.lopezl@gmail.com>
2020-05-05 10:25:31 -0600

    Initial commit

---
+++ src/test/java/com/david/timebox_time/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.timebox_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
